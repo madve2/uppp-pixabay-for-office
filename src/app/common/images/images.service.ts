6104f8b1c01c9a020a59dbdbf0397bf9790066fc
madve2 <madve2@gmail.com>
2017-10-18 16:53:11 +0200

    requests are cached for 24 hours as required by Pixabay

--- src/app/common/images/images.service.ts
+++ src/app/common/images/images.service.ts
@@ -11,18 +11,32 @@ import * as fromRoot from "../index";
 import { environment } from "../../../environments/environment";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/of';
+import lscache from 'lscache';
 
 @Injectable()
 export class ImagesService {
   constructor(private http: Http) {}
 
   getImages(request: {query: string, page: number}): Observable<any> {
+    var cacheKey = JSON.stringify(request);
+    let cachedResponse = lscache.get(cacheKey);
+    if (cachedResponse) {
+      if (!environment.production)
+        console.log("Found request in cache: " + cacheKey);
+      return Observable.of(cachedResponse);
+    }
+
     return this.http
       .request(
         `${environment.baseUrl}?key=${environment.apiKey}&q=${encodeURIComponent(request.query)}&page=${request.page}&image_type=photo`,
         { method: RequestMethod.Get }
       )
-      .map(response => response.json())
+      .map(response => {
+        let respJson = response.json();
+        lscache.set(cacheKey, respJson, 24 * 60);
+        return respJson;
+      })
       .catch(error => {
         if (!environment.production) console.log(error);
         return Observable.throw("Please try again later"); //TODO: can't actually get status code / error message due to CORS issue
